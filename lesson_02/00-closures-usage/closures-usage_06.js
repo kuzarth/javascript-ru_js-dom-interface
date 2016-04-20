49f99c0fb94cdcd354b505e654f838574fb9291d
Артур Кузь <recchimail@gmail.com>
2016-04-20 12:00:58 +0300

    lesson_02 (army of functions)

---
+++ lesson_02/00-closures-usage/closures-usage_06.js
@@ -0,0 +1,33 @@
+// Артур Кузь
+// Локальные переменные для объекта
+// Фильтрация через функцию
+
+(function() {
+  'use strict';
+  
+  function makeArmy() {
+
+    var shooters = [];
+
+    function createShooter(num) {
+      return function () {
+        return console.log(num);
+      }
+    }
+
+    for (var i = 0; i < 10; i++) {
+      var shooter = createShooter(i);
+
+      shooters.push(shooter);
+    }
+
+    return shooters;
+  }
+
+  var army = makeArmy();
+
+  army[0](); // стрелок выводит 10, а должен 0
+  army[5](); // стрелок выводит 10...
+  // .. все стрелки выводят 10 вместо 0,1,2...9
+
+})();

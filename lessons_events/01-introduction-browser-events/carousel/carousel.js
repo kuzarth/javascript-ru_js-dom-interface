30420d3486f11c2139dfd1f767ec96bb3725eed2
Артур Кузь <recchimail@gmail.com>
2016-05-30 12:52:35 +0300

    events - carousel

--- lessons_events/01-introduction-browser-events/carousel/carousel.js
+++ lessons_events/01-introduction-browser-events/carousel/carousel.js
@@ -1,10 +1,93 @@
-// этот код помечает картинки цифрами, для удобства разработки
-// его можно убрать, если не нужен
-var lis = document.getElementsByTagName('li');
-for (var i = 0; i < lis.length; i++) {
-    lis[i].style.position = 'relative';
-    var span = document.createElement('span');
-    span.style.cssText = 'position:absolute;left:0;top:0';
-    span.innerHTML = i + 1;
-    lis[i].appendChild(span);
+// var widget = document.querySelector('.js-widget');
+
+// var widgetItems = widget.querySelectorAll('li');
+// var widgetItemsQuantity = widgetItems.length;
+// var widgetItemWidth = widgetItems[0].offsetWidth;
+
+// var widgetContainer = widget.querySelector('.js-widget-container');
+// var widgetContainerWidth = widgetContainer.offsetWidth;
+// var widgetContainerPositionMax = (widgetItemsQuantity*widgetItemWidth) - widgetContainerWidth;
+// var widgetContainerPosition = 0;
+
+// console.log(widgetItemsQuantity, widgetContainerWidth, widgetItemWidth, widgetContainerPositionMax);
+
+// widget.onclick = function(e) {
+//     console.dir(e.target);
+//     console.log(e.target.dataset.dir);
+
+//     if(e.target.dataset.dir === "left") {
+//         console.log('slide left');
+
+//         if(widgetContainerPosition-widgetContainerWidth <= 0) {
+//             widgetContainerPosition = 0;
+//         } else {
+//             widgetContainerPosition -= widgetContainerWidth;
+//         }
+
+//         console.log(widgetContainerPosition);
+
+//         widgetContainer.querySelector('ul').style.transform = 'translate3d('+(-widgetContainerPosition)+'px , 0,  0)';
+//     }
+
+//     if(e.target.dataset.dir === "right") {
+//         console.log('slide right');
+        
+//         if(widgetContainerPosition+widgetContainerWidth >= widgetContainerPositionMax) {
+//             widgetContainerPosition = widgetContainerPositionMax;
+//         } else {
+//             widgetContainerPosition += widgetContainerWidth;
+//         }
+
+//         console.log(widgetContainerPosition);
+        
+//         widgetContainer.querySelector('ul').style.transform = 'translate3d('+(-widgetContainerPosition)+'px , 0,  0)';
+//     }
+// }
+
+'use strict';
+
+class Widget {
+    constructor(el) {
+        
+        this._el = el;
+
+        this._el.widgetItems = el.querySelectorAll('li');
+        this._el.widgetContainerPosition = 0;
+
+        this._el.widgetItemsQuantity = this._el.widgetItems.length;
+        this._el.widgetItemWidth = this._el.widgetItems[0].offsetWidth;
+
+        this._el.widgetContainer = el.querySelector('.js-widget-container');
+        this._el.widgetContainerWidth = this._el.widgetContainer.offsetWidth;
+        this._el.widgetContainerPositionMax = (this._el.widgetItemsQuantity * this._el.widgetItemWidth) - this._el.widgetContainerWidth;
+        
+        el.onclick = this.slide.bind(this);
+    }
+
+    slide(e) {
+
+        if(e.target.dataset.dir === "left") {
+
+            if(this._el.widgetContainerPosition - this._el.widgetContainerWidth <= 0) {
+                    this._el.widgetContainerPosition = 0;
+            } else {
+                this._el.widgetContainerPosition -= this._el.widgetContainerWidth;
+            }
+
+            this._el.widgetContainer.querySelector('ul').style.transform = 'translate3d('+(-this._el.widgetContainerPosition)+'px , 0,  0)';
+        }
+
+        if(e.target.dataset.dir === "right") {
+            
+            if(this._el.widgetContainerPosition + this._el.widgetContainerWidth >= this._el.widgetContainerPositionMax) {
+                this._el.widgetContainerPosition = this._el.widgetContainerPositionMax;
+            } else {
+                this._el.widgetContainerPosition += this._el.widgetContainerWidth;
+            }
+
+            this._el.widgetContainer.querySelector('ul').style.transform = 'translate3d('+(-this._el.widgetContainerPosition)+'px , 0,  0)';
+        }
+    }
 }
+
+let widget = new Widget(document.querySelector('.js-widget'));

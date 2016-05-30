28c62be26692c224ad8450d20f4bfc7093466dc0
Артур Кузь <recchimail@gmail.com>
2016-05-30 08:40:08 +0300

    events

---
+++ lessons_events/01-introduction-browser-events/carousel/carousel.js
@@ -0,0 +1,10 @@
+// этот код помечает картинки цифрами, для удобства разработки
+// его можно убрать, если не нужен
+var lis = document.getElementsByTagName('li');
+for (var i = 0; i < lis.length; i++) {
+    lis[i].style.position = 'relative';
+    var span = document.createElement('span');
+    span.style.cssText = 'position:absolute;left:0;top:0';
+    span.innerHTML = i + 1;
+    lis[i].appendChild(span);
+}
